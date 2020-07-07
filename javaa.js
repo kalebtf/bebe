388d8654a406d920331c112839aaf73c1d3f4c94
kalebtf <kalebtf@hotmail.com>
2020-07-06 19:26:48 -0500

    Add files via upload

---
+++ javaa.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    console.log('filter--->'+filter);
+
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+
+  console.log('filter--->'+filter);
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function msg(){  
+ alert("Hello Javatpoint");  
+}  
